db83680e7fd604b9d8600e16ce8053de5d22caa1
Sagar <sagar.devices@gmail.com>
2022-09-09 20:57:47 +0530

    integrated api

---
+++ index.js
@@ -0,0 +1,23 @@
+var busy = require('busyjs');
+var express = require('express');
+var app = express();
+var client = new busy.Client('wss://notifications.blurt.world');
+
+async function get_notifications(account) {
+  console.log('call notifications api', account);
+  return new Promise((resolve, reject) => {
+      client.call('get_notifications', [account], (err, result) => {
+          if (err !== null) reject(err);
+          resolve(result);
+      });
+  });
+}
+
+app.get('/get_notifications/:username', async function(req, res) {
+  var result = await get_notifications(req.params.username);
+  res.send(result);
+})
+
+app.listen((3000),()=>{
+  console.log("Server is Running")
+})
