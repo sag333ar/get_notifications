832fc7593b2e0c8e6466ceb9c464f20380774a67
Sagar <sagar.devices@gmail.com>
2022-09-09 23:27:58 +0530

    local client

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var busy = require('busyjs');
-var express = require('express');
+var busy = require("busyjs");
+var express = require("express");
 var app = express();
-var client = new busy.Client('wss://notifications.blurt.world');
 
 async function get_notifications(account) {
-  console.log('call notifications api', account);
+  console.log("call notifications api", account);
   return new Promise((resolve, reject) => {
-      client.call('get_notifications', [account], (err, result) => {
-          if (err !== null) reject(err);
-          resolve(result);
-      });
+    var client = new busy.Client("wss://notifications.blurt.world");
+    client.call("get_notifications", [account], (err, result) => {
+      if (err !== null) reject(err);
+      resolve(result);
+    });
   });
 }
 
-app.get('/get_notifications/:username', async function(req, res) {
+app.get("/get_notifications/:username", async function (req, res) {
   var result = await get_notifications(req.params.username);
   res.send(result);
-})
+});
 
-app.listen((5000),()=>{
-  console.log("Server is Running")
-})
+app.listen(5000, () => {
+  console.log("Server is Running");
+});
